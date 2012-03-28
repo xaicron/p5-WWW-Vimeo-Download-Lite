4b3157a245978f312b11018dd98bc3247805aa15
xaicron <xaicron@gmail.com>
2012-03-29 02:38:16 +0900

    initial commit

---
+++ lib/WWW/Vimeo/Download/Lite.pm
@@ -0,0 +1,42 @@
+package WWW::Vimeo::Download::Lite;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+WWW::Vimeo::Download::Lite -
+
+=head1 SYNOPSIS
+
+  use WWW::Vimeo::Download::Lite;
+
+=head1 DESCRIPTION
+
+WWW::Vimeo::Download::Lite is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
